c65122abc837737f3038b8218185eda585ae5a30
thigobaco <thigobaco@gmail.com>
2021-10-21 21:48:27 -0300

    Update snapCrackle.js
    Primeira tentativa para a Resolução da atividade proposta.

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,29 @@
-// Seu código vai aqui
+function scnapCrackle(maxValue){
+    let Msg = "";
+    for ( let Valor = 1; Valor <= maxValue; Valor ++){
+        if ( Valor %2 !== 0){
+            Msg += "Snap, "
+        }
+        if (Valor % 5 === 0){
+            Msg += "Crackle, "
+        }
+        if (Valor % 5 ===0 && Valor %2 !== 0){
+            Msg += "SnapCrackle, "
+        }
+        if (Valor %2 === 0 && Valor % 5 !==0){
+            Msg 
+        }
+        Msg += Valor + ", "           
+    } 
+        
+    console.log(Msg);
+}
+
+scnapCrackle(30)
+
+
+
+
+
+
+
