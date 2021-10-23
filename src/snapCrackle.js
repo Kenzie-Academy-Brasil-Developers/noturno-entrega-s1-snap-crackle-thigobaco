fc60cb243ef627e76eb21debc231134c65f6d5cb
thigobaco <thigobaco@gmail.com>
2021-10-23 20:56:25 -0300

    Refiz todo o código para realizar a entrega

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,29 +1,22 @@
-function snapCrackle(maxValue){
-    let Msg = "";
-    for ( let Valor = 1; Valor <= maxValue; Valor ++){
-        if ( Valor %2 !== 0){
-            Msg += "Snap, "
-        }
-        if (Valor % 5 === 0){
-            Msg += "Crackle, "
-        }
-        if (Valor % 5 ===0 && Valor %2 !== 0){
-            Msg += "SnapCrackle, "
-        }
-        if (Valor %2 === 0 && Valor % 5 !==0){
-            Msg 
-        }
-        Msg += Valor + ", "           
-    } 
-        
-    console.log(Msg);
+let primeira = "Snap";
+let segundaMsg = "Crackle";
+let terceiraMsg = "SnapCrackle";
+let maxValue = 12;
+let valor = 1;
+let saidaArr = [];
+function snapCrackle() {
+  while (valor <= maxValue) {
+    if (valor % 5 === 0 && valor % 2 !== 0) {
+      saidaArr.push(terceiraMsg);
+    } else if (valor % 5 === 0) {
+      saidaArr.push(segundaMsg);
+    } else if (valor % 5 === 0 || valor % 2 !== 1) {
+      saidaArr.push(valor);
+    } else if (valor % 2 !== 0) {
+      saidaArr.push(primeira);
+    }
+    valor++;
+  }
+  return saidaArr // para retornar a saida do meu array nas codições
 }
-
-snapCrackle(12)
-
-
-
-
-
-
-
+console.log(snapCrackle().join()); // para retornar a junção da matriz.(.join) tive que pesquisar sobre um método que me desse esse retorno https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/join
